b5528d8c1d62695c4c6d8e4ddb229e7b67eeac87
Metro <jessicametro@gmail.com>
2013-01-27 22:25:58 -0600

    header disappears and reappears on scroll
    but without animations

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,15 @@ $(window).ready(function() {
     var lastTop = 0;  // the last px at the top of the browser
     $(window).scroll(function() {
     	var top = $(window).scrollTop();  // tells you where the page is
-    	if(top < 0) {
+    	if (top < 0) {
     		top = 0; // compensates for bouncing
     	}
     	var direction = top - lastTop;
+    	if (direction < 0) {  // if scrolling up
+    		header.css('margin-top', '0px'); // show header (convoluted format)
+    	} else if (direction > 0) {  // if scrolling down
+    		header.css('margin-top', '-60px');  // hide header
+    	}
     	
     	console.log(top - lastTop);  // tells you the direction
     	    // whenever number is positive you're scrolling down 
